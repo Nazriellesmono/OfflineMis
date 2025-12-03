a2f19db8ac2dd0ae54dcce860dbb143ae01644ad
Nazriellesmono <nazrielaril14@gmail.com>
2025-12-03 21:02:15 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Middleware\PreventBackHistory;
 use App\Http\Controllers\FrsController;
 use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\KuesionerController;
